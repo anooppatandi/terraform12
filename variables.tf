a0688ffaa518f00b04d03c95fedcba6d1b6fe8da
anooppatandi <anoop.p.eee@gmail.com>
2020-10-03 23:24:37 +0530

    commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+
+  default = "ap-south-1"
+
+}
+
+variable "ami" {
+
+  default = "ami-0e306788ff2473ccb"
+
+}
+
+variable "ec2key"{
+
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDvv0nmOzV6YBqQoFZlUiGkLMVDQWrjwxB33dhsmQdmB5KvWeGXpIPtdnX5gtUcxAzfodQbxw3sXEbYHwqWBV47yfOYdwaWpfv+QvYUDATNv5sf9t/51ZYncbzZu7U21w9AqIjU3Yf2ZcHoDwVAfPiIxo+dzTrVu+DsFmaiWQFnuwaa49l5UNIKhnhPLVJ8jXF9BbDQB8s+aN4+I3KF6baEsZDySTczo2eL4VIjZ3BYIVry9ClG6nxzjHyD8BTsAF88ynRTPZv65iXOgDUD/iDwThuP9rk1afYcgETKstmn54TWQi42XWsObQiObpOMN5NgUl70E3XQzXj7FUtfNV7zjbFp6L7n6Lb7OvHoOvfXD5ENw3jntYrhX8NR+ADbG+WOMxUK4A2CRzrWdX5RuCBlt1LUL+q4LDGmKrrHwUMDXd2ZTPr52ImxF+zRwQgkLOXKkGrdvO7Op9Mz4apOCuRPMj1+lrq2Z3jLDNyDZyVpD2x8J5rTxdS+T8aeN7GJmw8= anoopp@HYD005000434"
+
+}
